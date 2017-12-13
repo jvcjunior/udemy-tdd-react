6c051d497c29a42ec42174b288c80ef18db62443
Valter <bloodsportjr@gmail.com>
2017-12-13 14:24:04 -0300

    add cookie test

--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -1,22 +1,31 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-    it('sets a balance', () => {
+    describe('when initializing', () => {
         const balance = 10;
-        const action = { type: constants.SET_BALANCE, balance };
+        it('sets a balance', () => {
+            const action = { type: constants.SET_BALANCE, balance };
 
-        expect(balanceReducer(undefined, action))
-            .toEqual(balance);
-    });
+            expect(balanceReducer(undefined, action))
+                .toEqual(balance);
+        });
 
-    it('deposits into the balance', () => {
-        const initialState = 5;
-        const deposit = 10;
-        const action = { type: constants.DEPOSIT, deposit };
+        describe('then re-initializing', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReducer2(undefined, {})).toEqual(balance);
+            })
+        });
 
-        expect(balanceReducer(initialState, action))
-            .toEqual(deposit+initialState);
+        it('deposits into the balance', () => {
+            const initialState = 5;
+            const deposit = 10;
+            const action = { type: constants.DEPOSIT, deposit };
+
+            expect(balanceReducer(initialState, action))
+                .toEqual(deposit+initialState);
+        });
     });
 
     it('withdraw from the balance', () => {
